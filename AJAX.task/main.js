0396d9629ff5093fffb5440ccd4dfd7d0efbac16
MirheydarA <121608999+MirheydarA@users.noreply.github.com>
2023-05-08 15:22:27 +0400

    Add files via upload

--- AJAX.task/main.js
+++ AJAX.task/main.js
@@ -1,20 +1,28 @@
-$("#btn").click( () => {
-    $.ajax({
-        method: "GET",
-        url: "https://jsonplaceholder.typicode.com/users",
-        headers: {
-            "auth-token": "token"
-        },
-        success: (data) => {
-            $.each(data, (_,value) => {
-                $("div").append(`<a>${value.name}</a>`)
-                $("a").addClass("list-group-item list-group-item-action")
-                
-                $("a").click( () => {
-                    console.log(value.name);
-                })
-            }
-            )}
+$("#btn").click(() => {
+  $.ajax({
+    method: "GET",
+    url: "https://jsonplaceholder.typicode.com/users",
+    headers: {
+      "auth-token": "token",
+    },
+    success: (data) => {
+      $.each(data, (_, value) => {
+        $("div").append(`<a>${value.name}</a>`);
+        $("a").addClass("list-group-item list-group-item-action");
+      });
+    },
+  });
+});
 
-    })
-})
+document.addEventListener("click", function (event) {
+  let btn = document.getElementById("btn")
+  let text;
+  event.preventDefault();
+  text = event.target.innerText;
+  if (
+      event.target.className.includes("list-group-item list-group-item-action")
+      ) {
+          alert(event.target.innerText);
+        }
+        btn.style.display = "none"
+});
